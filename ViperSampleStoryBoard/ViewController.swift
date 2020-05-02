c27f750c9876a90c8da6d6a01e2c6933a4f1e3f3
Ryuji Muraoka <r-muraoka@semo.co.jp>
2020-05-02 22:24:18 +0900

    Initial Commit

---
+++ ViperSampleStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperSampleStoryBoard
+//
+//  Created by 村岡龍治 on 2020/05/02.
+//  Copyright © 2020 村岡龍治. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
